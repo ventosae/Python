f09e3e732717dfa9664c7415ac860e476e01ed7c
ventosae <anton.surov@hotmail.com>
2017-09-11 22:37:50 +0100

    profanity program finish

--- check_profanity.py
+++ check_profanity.py
@@ -1,6 +1,23 @@
+import urllib.request
+
 def read_text():
-    q= open("C:\study_files\quotes.txt")
-    contents = q.read()
-    print(contents)
-    q.close()
+    quotes = open("C:\study_files\quotes.txt")
+    contents_of_file = quotes.read()
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    query = urllib.parse.urlencode({'q': text_to_check})
+    url = "http://www.wdylike.appspot.com/?"+query
+    connection = urllib.request.urlopen(url)
+    output= str(connection.read())
+##    print(output)
+    if "true" in output:
+        print("All good man")
+    elif "false" in output:
+        print("Something worng")
+    else:
+        print("Error")
+    connection.close()
+    
 read_text()
